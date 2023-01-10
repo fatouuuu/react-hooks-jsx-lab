67431a59b3d8581ef070e2fd35798ce3fac1a493
fatouuuu <fatmaabdullrahim@gmail.com>
2023-01-10 12:36:07 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+        <h2>About Me</h2>
+        <p> I am a web developer with a passion for creating elegant, functional websites and web applications. I enjoy working with modern technologies like React, and Vue.js. I'm driven by a desire to create great user experiences and improve my skills every day. I am always keen to take on challenging projects and look forward to working with a team of highly motivated people. </p>
+        <img src={image} alt="I made this" />
+    </div>
+);
 }
 
 export default About;
